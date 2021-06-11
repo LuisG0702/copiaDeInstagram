8786a6ecd9fb67a02fdb352498b94c95755ea363
Henry Guzmán <hguzman10@gmail.com>
2021-06-10 20:43:21 -0500

    Ajustes la carpeta en el lugar correcto y ubique el scroll

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:practica/Pages/home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'App sena',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
--- practica/lib/models/stories_model.dart
+++
@@ -1,9 +0,0 @@
-class Storie {
-  String name;
-  String photo;
-
-  Storie({
-    this.name,
-    this.photo,
-  });
-}
